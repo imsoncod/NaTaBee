6af36cf1f893d04dddb31432464f0f59fe93a4d8
iustron@naver.com <iustron@naver.com>
2020-10-14 17:15:46 +0900

    [Init] Swagger 설정 파일

---
+++ routes/swagger.js
@@ -0,0 +1,82 @@
+/**
+ * @swagger
+ * definitions:
+ *   request_basic:
+ *     type: object
+ *     properties:
+ *       intent:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: string
+ *           name:
+ *             type: string 
+ *       userRequest:
+ *         type: object
+ *         properties:
+ *           timezone:
+ *             type: string
+ *           params:
+ *             properties:
+ *               ignoreMe:
+ *                 type: string
+ *           block:
+ *             properties:
+ *               id:
+ *                 type: string
+ *               name:
+ *                 type: string  
+ *           utterance:
+ *             type: string
+ *           lang:
+ *             type: string
+ *           user:      
+ *             properties:
+ *               id:
+ *                 type: string
+ *               type:
+ *                 type: string
+ *               properties:
+ *                 type: object  
+ *       bot:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: string
+ *           name:
+ *             type: string
+ *       action:
+ *         type: object
+ *         properties:
+ *           name:
+ *             type: string
+ *           clientExtra:
+ *             type: string
+ *           params:
+ *             type: object
+ *           id:
+ *             type: string
+ *           detailParams:
+ *             type: object
+ */
+
+//Swagger
+module.exports = {
+		  swaggerDefinition: {
+		    // 정보
+		    info: {
+		      title: 'NaTaBee',
+		      version: '1.0.0',
+		      description: '인하공전 도우미 챗봇 NaTaBee API'
+		    },
+		    // 주소
+		    host: "localhost:8087",
+		    // 기본 root path
+		    basePath: "/natabee",
+		    contact: {
+		      email: "iustron@naver.com"
+		    },
+		    schemes: ["http", "https"] // 가능한 통신 방식
+		  },
+		  apis: ['./routes/*.js'] // api 파일 위치들 
+		};
